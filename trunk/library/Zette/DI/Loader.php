f832dafad42eccd6e54dd74f89cafbf48edabfeb
xzabm00 <xzabm00@ea3fb1bf-ebfe-4ede-80e0-45d1aed49344>
2012-10-14 22:10:02 +0000

    zette di
    git-svn-id: https://4it445.vse.cz/svn/zs1213_c@5 ea3fb1bf-ebfe-4ede-80e0-45d1aed49344

---
+++ trunk/library/Zette/DI/Loader.php
@@ -0,0 +1,22 @@
+<?php
+namespace Zette\DI;
+
+/**
+ * Created by JetBrains PhpStorm.
+ * User: misak113
+ * Date: 15.10.12
+ * Time: 0:22
+ * To change this template use File | Settings | File Templates.
+ */
+class Loader
+{
+
+	public function load() {
+
+	}
+
+	public function getContext() {
+		return null;
+	}
+
+}
