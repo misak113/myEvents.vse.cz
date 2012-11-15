b304cd01aa093acf6ce8a7414e6c6e3c1f2d8dda
xzabm00 <xzabm00@ea3fb1bf-ebfe-4ede-80e0-45d1aed49344>
2012-11-15 21:33:43 +0000

    landing page
    git-svn-id: https://4it445.vse.cz/svn/zs1213_c@17 ea3fb1bf-ebfe-4ede-80e0-45d1aed49344

---
+++ trunk/application/modules/landing/controllers/IndexController.php
@@ -0,0 +1,36 @@
+<?php
+
+use Zette\UI\BaseController;
+
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Michael
+ * Date: 15.11.12
+ * Time: 22:44
+ * To change this template use File | Settings | File Templates.
+ */
+class IndexController extends BaseController
+{
+	const FILE_EMAILS = 'emails.txt';
+
+	public function indexAction() {
+
+	}
+
+	public function emailSentAction() {
+
+		// @todo
+		$email = @$_POST['email'];
+
+		$status = @file_put_contents(FILE_EMAILS, $email.";".time().";".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND);
+
+		if ($status) {
+			header('Location: /');
+			setcookie('email_sent', 'sent', time()+5);
+		} else {
+			header('Location: /');
+		}
+
+	}
+
+}
