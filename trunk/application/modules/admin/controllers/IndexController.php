794cdcb376b91fb7b715bd67aa87895895817364
xtomf00 <xtomf00@ea3fb1bf-ebfe-4ede-80e0-45d1aed49344>
2012-11-25 17:16:01 +0000

    Přidány administrátoské routy a základní prolinkování admin modulu
    git-svn-id: https://4it445.vse.cz/svn/zs1213_c@57 ea3fb1bf-ebfe-4ede-80e0-45d1aed49344

--- trunk/application/modules/admin/controllers/IndexController.php
+++ trunk/application/modules/admin/controllers/IndexController.php
@@ -1,6 +1,7 @@
 <?php
 
 use Zette\UI\BaseController;
+use app\services\TitleLoader;
 
 /**
  * Created by JetBrains PhpStorm.
@@ -9,11 +10,32 @@ use Zette\UI\BaseController;
  * Time: 16:39
  * To change this template use File | Settings | File Templates.
  */
-class Admin_IndexController extends BaseController
-{
+class Admin_IndexController extends BaseController {
+    
+    /** @var TitleLoader */
+    protected $titleLoader;
 
-	public function indexAction() {
+    /**
+     * @param TitleLoader $titleLoader 
+     */
+    public function setContext(TitleLoader $titleLoader) {
+        $this->titleLoader = $titleLoader;
+    }
 
-	}
+    public function indexAction() {
+        $this->template->title = $this->titleLoader->getTitle('Admin:Index:index');
+    }
+
+    public function loginAction() {
+        $this->template->title = $this->titleLoader->getTitle('Admin:Index:login');
+    }
+    
+    public function editAction() {
+        $this->template->title = $this->titleLoader->getTitle('Admin:Index:new');
+    }
+    
+    public function adminAction() {
+        $this->template->title = $this->titleLoader->getTitle('Admin:Index:admin');
+    }
 
 }
