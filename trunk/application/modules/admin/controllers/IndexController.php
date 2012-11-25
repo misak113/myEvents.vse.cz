fa66fc7b4f539098e4b6cdd0bdb1f371f5c17f1f
xtomf00 <xtomf00@ea3fb1bf-ebfe-4ede-80e0-45d1aed49344>
2012-11-25 18:36:23 +0000

    Přidána dummy data do administračního modulu. Akce lze nyní vytvářet a editovat, ale nikam se nic neuloží.
    git-svn-id: https://4it445.vse.cz/svn/zs1213_c@58 ea3fb1bf-ebfe-4ede-80e0-45d1aed49344

--- trunk/application/modules/admin/controllers/IndexController.php
+++ trunk/application/modules/admin/controllers/IndexController.php
@@ -14,6 +14,8 @@ class Admin_IndexController extends BaseController {
     
     /** @var TitleLoader */
     protected $titleLoader;
+    
+    protected $eventPrototype;
 
     /**
      * @param TitleLoader $titleLoader 
@@ -21,9 +23,9 @@ class Admin_IndexController extends BaseController {
     public function setContext(TitleLoader $titleLoader) {
         $this->titleLoader = $titleLoader;
     }
-
-    public function indexAction() {
-        $this->template->title = $this->titleLoader->getTitle('Admin:Index:index');
+    
+    public function init(){
+        $this->eventPrototype = Event::create();
     }
 
     public function loginAction() {
@@ -31,11 +33,96 @@ class Admin_IndexController extends BaseController {
     }
     
     public function editAction() {
+        $eventId = $this->_getParam('id');
+        
         $this->template->title = $this->titleLoader->getTitle('Admin:Index:new');
+        if($eventId){
+            $this->view->event = $this->eventPrototype[$eventId];
+        } else {
+            $this->view->event = new Event();
+        }
     }
     
-    public function adminAction() {
-        $this->template->title = $this->titleLoader->getTitle('Admin:Index:admin');
+    public function indexAction() {
+        $this->template->title = $this->titleLoader->getTitle('Admin:Index:index');
+        $this->template->events = $this->eventPrototype;
     }
 
+}
+
+class Event{
+    public $fbUrl;
+    public $nazev;
+    public $datum;
+    public $zacatek;
+    public $konec;
+    public $misto;
+    public $kapacita;
+    public $popis;
+    //TODO: obrazek;
+    public $sponzor;
+    
+    public static function create(){
+        $events[1] = new Event();
+        $events[1]->id = 1;
+        $events[1]->datum = "1.11.2012";
+        $events[1]->fbUrl = "https://www.facebook.com/myEvents.vse.cz";
+        $events[1]->kapacita = 300;
+        $events[1]->konec = "20:00";
+        $events[1]->misto = "RB 101";
+        $events[1]->nazev = "Setkání filatelistů 1";
+        $events[1]->popis = "Setkání filatelistů v RB 101. Bude sranda.";
+        $events[1]->sponzor = "";
+        $events[1]->zacatek = "19:00";
+        
+        $events[2] = new Event();
+        $events[2]->id = 2;
+        $events[2]->datum = "5.11.2012";
+        $events[2]->fbUrl = "https://www.facebook.com/myEvents.vse.cz";
+        $events[2]->kapacita = 300;
+        $events[2]->konec = "20:00";
+        $events[2]->misto = "RB 101";
+        $events[2]->nazev = "Jak fungují filateistické burzy";
+        $events[2]->popis = "Setkání filatelistů v RB 101. Bude sranda.";
+        $events[2]->sponzor = "";
+        $events[2]->zacatek = "19:00";
+        
+        $events[3] = new Event();
+        $events[3]->id = 3;
+        $events[3]->datum = "8.11.2012";
+        $events[3]->fbUrl = "https://www.facebook.com/myEvents.vse.cz";
+        $events[3]->kapacita = 300;
+        $events[3]->konec = "20:00";
+        $events[3]->misto = "RB 101";
+        $events[3]->nazev = "Setkání filatelistů 2";
+        $events[3]->popis = "Setkání filatelistů v RB 101. Bude sranda.";
+        $events[3]->sponzor = "";
+        $events[3]->zacatek = "19:00";
+        
+        $events[4] = new Event();
+        $events[4]->id = 4;
+        $events[4]->datum = "12.11.2012";
+        $events[4]->fbUrl = "https://www.facebook.com/myEvents.vse.cz";
+        $events[4]->kapacita = 300;
+        $events[4]->konec = "20:00";
+        $events[4]->misto = "RB 101";
+        $events[4]->nazev = "Po stopách modrého mauricia";
+        $events[4]->popis = "Setkání filatelistů v RB 101. Bude sranda.";
+        $events[4]->sponzor = "";
+        $events[4]->zacatek = "19:00";
+        
+        $events[5] = new Event();
+        $events[5]->id = 5;
+        $events[5]->datum = "30.11.2012";
+        $events[5]->fbUrl = "https://www.facebook.com/myEvents.vse.cz";
+        $events[5]->kapacita = 300;
+        $events[5]->konec = "20:00";
+        $events[5]->misto = "RB 101";
+        $events[5]->nazev = "Setkání filatelistů 3";
+        $events[5]->popis = "Setkání filatelistů v RB 101. Bude sranda.";
+        $events[5]->sponzor = "";
+        $events[5]->zacatek = "19:00";
+        
+        return $events;
+    }
 }
