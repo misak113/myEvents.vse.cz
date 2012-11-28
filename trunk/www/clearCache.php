c937dc12148d0bb6c867f043fa2567336e790a9e
xzabm00 <xzabm00@ea3fb1bf-ebfe-4ede-80e0-45d1aed49344>
2012-11-28 09:51:41 +0000

    oprava clearCache
    git-svn-id: https://4it445.vse.cz/svn/zs1213_c@73 ea3fb1bf-ebfe-4ede-80e0-45d1aed49344

--- trunk/www/clearCache.php
+++ trunk/www/clearCache.php
@@ -1,12 +1,30 @@
 <?php
 
+header('content-type: text/plain;charset=utf-8');
+
+function destroy_dir($dir) { 
+    if (!is_dir($dir) || is_link($dir)) return unlink($dir); 
+        foreach (scandir($dir) as $file) { 
+            if ($file == '.' || $file == '..') continue; 
+            if (!destroy_dir($dir . DIRECTORY_SEPARATOR . $file)) { 
+                chmod($dir . DIRECTORY_SEPARATOR . $file, 0777); 
+                if (!destroy_dir($dir . DIRECTORY_SEPARATOR . $file)) return false; 
+            }; 
+        } 
+        return rmdir($dir); 
+    } 
+
+
 $cacheDir = realpath("../temp/cache");
 
+
 if (is_dir($cacheDir)) {
-	$status = @rmdir($cacheDir);
+	$status = destroy_dir($cacheDir);
 	if ($status) {
 		echo "Cache '".$cacheDir."' byla úspěšně smazána";
 	} else {
 		echo "Nastala chyba při mazání cache: '".$cacheDir."'";
 	}
+} else {
+	echo "Cache v umístění '".$cacheDir."' neexistuje!";
 }
