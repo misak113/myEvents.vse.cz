041249fd2b1deaa16d392e122eeb160aafcc6943
xzabm00 <xzabm00@ea3fb1bf-ebfe-4ede-80e0-45d1aed49344>
2012-12-02 23:12:25 +0000

    cisteni
    git-svn-id: https://4it445.vse.cz/svn/zs1213_c@109 ea3fb1bf-ebfe-4ede-80e0-45d1aed49344

--- trunk/www/js/Modules/landing/messages.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function () {
-	$('.messages').fadeIn();
-	setTimeout(function () {
-		$('.messages').fadeOut();
-	}, 5000);
-});
--- trunk/www/js/components/Filter/filter.js
+++ trunk/www/js/components/Filter/filter.js
@@ -25,5 +25,4 @@ jQuery(document).ready(function ($) {
 		filterForm.trigger('submit');
 	});
 
-
 });
