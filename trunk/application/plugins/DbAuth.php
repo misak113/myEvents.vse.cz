9e0b0eeb4c4bb1bb7a7e862acccbd18b382d6f44
xmacj64 <xmacj64@ea3fb1bf-ebfe-4ede-80e0-45d1aed49344>
2012-12-03 02:17:58 +0000

    git-svn-id: https://4it445.vse.cz/svn/zs1213_c@120 ea3fb1bf-ebfe-4ede-80e0-45d1aed49344

--- trunk/application/plugins/DbAuth.php
+++ trunk/application/plugins/DbAuth.php
@@ -6,7 +6,7 @@
  *
  * @see Zend_Auth_Adapter_DbTable
  */
-class Application_Plugins_DbAuth extends Zend_Controller_Plugin_Abstract {
+class Plugins_DbAuth extends Zend_Controller_Plugin_Abstract {
 
     /**
      *
