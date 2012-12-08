bb15c73aaf158c714d485818d69269dd41875854
xzabm00 <xzabm00@ea3fb1bf-ebfe-4ede-80e0-45d1aed49344>
2012-12-08 16:56:43 +0000

    DI na USer TAblesvn status
    git-svn-id: https://4it445.vse.cz/svn/zs1213_c@215 ea3fb1bf-ebfe-4ede-80e0-45d1aed49344

---
+++ trunk/application/services/User.php
@@ -0,0 +1,26 @@
+<?php
+namespace app\services;
+
+use Nette\Security\User as NetteUser;
+
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Michael
+ * Date: 8.12.12
+ * Time: 17:44
+ * To change this template use File | Settings | File Templates.
+ */
+class User extends NetteUser
+{
+
+	public function __construct(\Nette\Security\IUserStorage $storage, \Nette\DI\Container $context) {
+		parent::__construct($storage, $context);
+		/** @var \app\models\authorization\RoleTable $roleTable  */
+		$roleTable = $context->getService('roleTable');
+		$authenticatedRole = $roleTable->getOrCreateRole($this->authenticatedRole);
+		$guestRole = $roleTable->getOrCreateRole($this->guestRole);
+		$this->authenticatedRole = $authenticatedRole ?$authenticatedRole->toArray() :null;
+		$this->guestRole = $guestRole ?$guestRole->toArray() :null;
+	}
+
+}
