479d94a6b15d80a2cca38a461e585995af3cae68
xkler07 <xkler07@ea3fb1bf-ebfe-4ede-80e0-45d1aed49344>
2012-12-12 16:18:39 +0000

    Seznam organizací, se kterými spolupracujeme (reálná data - název organizace + fbid)
    git-svn-id: https://4it445.vse.cz/svn/zs1213_c@270 ea3fb1bf-ebfe-4ede-80e0-45d1aed49344

--- trunk/data/testData.sql
+++ trunk/data/testData.sql
@@ -1,4 +1,4 @@
--- Adminer 3.6.1 MySQL dump
+﻿-- Adminer 3.6.1 MySQL dump
 
 SET NAMES utf8;
 SET foreign_key_checks = 0;
@@ -17,12 +17,32 @@ DELETE FROM `organization`;
 -- Dumping data for table `organization`
 --
 
-INSERT INTO `organization` (`organization_id`, `name`, `website`, `info`, `email`, `logo`, `fb_url`) VALUES
-(1, 'Business IT', NULL, NULL, NULL, 'BizIT.png', NULL),
-(2, 'Klub koučinku', NULL, NULL, NULL, 'KlubKoucinku.png', NULL),
-(3, 'Klub mladých manažerů', 'http://www.kmm.cz/', 'Studentský univerzitní klub se zaměřením na management. Pořádáme setkání se zkušenými manažery a podnikateli z praxe.\r\n\r\n\r\nSpojujeme manažerské generace\r\n\r\n\r\nDíky setkáním s manažery a podnikateli předáváme informace, životní postoje a zkušenosti mezi potenciálními a začínajícími manažery a lidmi z praxe. Mimo jiné pořádáme pro členy klubu zajímavé workshopy a neformální akce. Klub mladých manažerů operuje pod záštitou České manažerské asociace. Více info na http://www.kmm.cz/', 'manazeri@man.cz', 'KMM.png', 'https://www.facebook.com/klubmladychmanazeru'),
-(4, 'AIESEC', NULL, NULL, NULL, 'AIESEC.png', NULL),
-(5, 'Sport.vse.cz', NULL, NULL, NULL, 'SportVSE.png', NULL);
+INSERT INTO `organization` (`organization_id`, `name`, `website`, `info`, `email`, `logo`, `fb_url`, `facebook_id`) VALUES
+(1, 'BizIT', '', '', '', '', '', '93602669985'),
+(2, 'Klub koučinku', '', '', '', '', '', '296052990420437'),
+(3, 'Cashflow Klub', '', '', '', '', '', ''),
+(4, 'AIESEC', '', '', '', '', '', '95679730987'),
+(5, 'Sport.VSE.cz', '', '', '', '', '', ''),
+(6, '180 Degrees Consulting', '', '', '', '', '', '345051448913246'),
+(7, 'AEGEE-PRAHA', '', '', '', '', '', '176721839052'),
+(9, 'Alfa fí AΦ', '', '', '', '', '', '205013259510059'),
+(13, 'Club 307', '', '', '', '', '', '219079248152830'),
+(14, 'Ekonom, Economix.cz', '', '', '', '', '', '359121546135'),
+(15, 'ESN VŠE Praha - Buddy System', '', '', '', '', '', '358352857511810'),
+(16, 'FRIENDS VŠE', '', '', '', '', '', '216388031716386'),
+(17, 'Golf Networking Club', '', '', '', '', '', '173903462657747'),
+(18, 'iKnow Club, iKnow.eu', '', '', '', '', '', '103296207540'),
+(19, 'Indivisuals', '', '', '', '', '', '168458376537044'),
+(20, 'Institut energetické ekonomie', '', '', '', '', '', '191589684197152'),
+(21, 'Jeden svět na VŠE', '', '', '', '', '', '165736606778774'),
+(22, 'Klub Investorů', '', '', '', '', '', ''),
+(24, 'Klub mladých logistiků', '', '', '', '', '', ''),
+(25, 'Klub mladých politologů', '', '', '', '', '', '118792701230'),
+(26, 'KMM', '', '', '', '', '', '107800475917180'),
+(27, 'Marketing and media club VŠE', '', '', '', '', '', '437624672941595'),
+(28, 'Marketing club', '', '', '', '', '', '10150114835185591'),
+(29, 'Model UN Prague', '', '', '', '', '', '477237812316043'),
+(31, 'Studentský klub pro projektové řízení', NULL, NULL, NULL, NULL, NULL, NULL);
 
 
 -- Adminer 3.6.1 MySQL dump
