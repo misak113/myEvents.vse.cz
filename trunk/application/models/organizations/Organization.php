c087558e6768c41bb852628dd19b8924fe6fe2cc
xmacj64 <xmacj64@ea3fb1bf-ebfe-4ede-80e0-45d1aed49344>
2012-12-15 11:21:02 +0000

    - doplnění kontaktních uživatelů do přehledu organizací
    git-svn-id: https://4it445.vse.cz/svn/zs1213_c@284 ea3fb1bf-ebfe-4ede-80e0-45d1aed49344

--- trunk/application/models/organizations/Organization.php
+++ trunk/application/models/organizations/Organization.php
@@ -1,18 +1,27 @@
 <?php
+
 namespace app\models\organizations;
 
 use My\Db\Table\Row;
 
-class Organization extends Row
-{
+class Organization extends Row {
+
     /**
      * 
      * @return Zend_Db_Table_Rowset_Abstract
      */
     public function getEvents() {
-            return $this->findManyToManyRowset('app\models\events\EventTable', 'app\models\organizations\OrganizationOwnEventTable');
+        return $this->findManyToManyRowset('app\models\events\EventTable', 'app\models\organizations\OrganizationOwnEventTable');
     }
-    
+
+    public function getContactUser() {
+        $userTable = \My_Model::get("app\models\organizations\OrganizationHasUserTable");
+        $select = $userTable->select()->where("i.member = 2");
+        $rowset = $this->findManyToManyRowset('app\models\authentication\UserTable', "app\models\organizations\OrganizationHasUserTable", null, null, $select);
+        return $rowset->current();
+        
+    }
+
     /**
      * Update Organization
      * @param array $values
@@ -20,10 +29,10 @@ class Organization extends Row
      */
     public function updateFromArray(array $values) {
 
-            $this->setFromArray($values);
-            $this->save();
+        $this->setFromArray($values);
+        $this->save();
 
-            return $this;
+        return $this;
     }
 
 }
