528da90817fd066b43f03e2a577225de56648582
xzabm00 <xzabm00@ea3fb1bf-ebfe-4ede-80e0-45d1aed49344>
2012-12-26 11:00:24 +0000

    dalsi oprava dates filter
    git-svn-id: https://4it445.vse.cz/svn/zs1213_c@338 ea3fb1bf-ebfe-4ede-80e0-45d1aed49344

--- trunk/application/components/Filter/FilterDispatcher.php
+++ trunk/application/components/Filter/FilterDispatcher.php
@@ -128,34 +128,8 @@ _dBar($events);
 		return $filter;
 	}
 
-	/**
-	 * Vygeneruje semestrální i mimosemestrální týdny pro dates filter
-	 * @return array
-	 */
-	protected function generateDates() {
-		$weeks = $this->getSemestralWeeks();
 
-		if (empty($weeks)) {
-			$from = $this->getDate()->format('W');
-			$weeks = $this->getSemestralWeeks(
-				$from >= self::WEEK_START_NUMBER_ZIMNI + self::WEEKS_SEMESTRAL_COUNT && $from <= self::WEEK_START_NUMBER_LETNI
-					?self::WEEK_START_NUMBER_LETNI
-					:self::WEEK_START_NUMBER_ZIMNI
-			);
-		} else {
-			$last = end($weeks);
-			$from = ($last['end']->format('W')+1)%self::WEEKS_YEAR_COUNT;
-		}
-		$weeks += $this->getMimosemestralWeeks($from);
 
-		uasort($weeks, function ($a, $b) {
-			return $a['start']->getTimestamp() > $b['start']->getTimestamp() ?1 :-1;
-		});
-
-		$weeks = array_slice($weeks, 0, 6, true);
-
-		return $weeks;
-	}
 
 
 
@@ -164,63 +138,37 @@ _dBar($events);
 	const WEEKS_SEMESTRAL_COUNT = 13;
 	const WEEKS_YEAR_COUNT = 53;
 
-	/**
-	 * Vygeneruje data pro filter a to následující týdny výuky
-	 * @return array
-	 */
-	protected function getSemestralWeeks($weekNow = false) {
-		$now = $this->getDate();
-		if ($weekNow === false) $weekNow = $now->format('W');
-		$firstWeekNumber = false;
-		if ($weekNow >= self::WEEK_START_NUMBER_LETNI && $weekNow < self::WEEK_START_NUMBER_LETNI + self::WEEKS_SEMESTRAL_COUNT) {
-			$firstWeekNumber = self::WEEK_START_NUMBER_LETNI;
-		} elseif ($weekNow >= self::WEEK_START_NUMBER_ZIMNI && $weekNow < self::WEEK_START_NUMBER_ZIMNI + self::WEEKS_SEMESTRAL_COUNT) {
-			$firstWeekNumber = self::WEEK_START_NUMBER_ZIMNI;
-		}
-
-		// Mimo semestr
-		if ($firstWeekNumber === false) {
-			return array();
-		}
-
-		$weekNumber = $firstWeekNumber;
+	protected function generateDates() {
 		$weeks = array();
-		for ($schoolWeek = 1;$schoolWeek <= self::WEEKS_SEMESTRAL_COUNT;$schoolWeek++,$weekNumber++) {
-			if ($weekNumber >= $now->format('W')) {
-				$start = $this->createDateTimeFromWeek($weekNumber);
-				$end = $this->createDateTimeFromWeek($weekNumber+1);
-				if (!$start || !$end) continue;
+		$weekNumber = $this->getDate('W');
+		for ($i=0;$i < 7;$i++,$weekNumber = ($weekNumber+1) % self::WEEKS_YEAR_COUNT) {
+			$start = $this->createDateTimeFromWeek($weekNumber);
+			$end = $this->createDateTimeFromWeek($weekNumber+1)->sub(\DateInterval::createFromDateString('1 days'));
+			if (
+				($weekNumber >= self::WEEK_START_NUMBER_LETNI && $weekNumber < self::WEEK_START_NUMBER_LETNI + self::WEEKS_SEMESTRAL_COUNT)
+				|| ($weekNumber >= self::WEEK_START_NUMBER_ZIMNI && $weekNumber < self::WEEK_START_NUMBER_ZIMNI + self::WEEKS_SEMESTRAL_COUNT)
+			)
+			{
+				$schoolWeek =
+						$weekNumber - self::WEEK_START_NUMBER_ZIMNI < self::WEEKS_SEMESTRAL_COUNT && $weekNumber - self::WEEK_START_NUMBER_ZIMNI >= 0
+								?$weekNumber - self::WEEK_START_NUMBER_ZIMNI+1
+								:$weekNumber - self::WEEK_START_NUMBER_LETNI+1;
 				$week = array(
-					'id' => $schoolWeek,
+					'id' => $weekNumber,
 					'title' => $schoolWeek.'. Týden ('.$start->format('j.n.').'-'.$end->format('j.n.').')',
 					'week_number' => $weekNumber,
 					'start' => $start,
-					'end' => $end->sub(\DateInterval::createFromDateString('1 days')),
+					'end' => $end,
 				);
-				$weeks[$weekNumber] = $week;
+			} else {
+				$week = array(
+					'id' => $weekNumber,
+					'title' => $start->format('j.n.').' - '.$end->format('j.n.'),
+					'week_number' => $weekNumber,
+					'start' => $start,
+					'end' => $end,
+				);
 			}
-		}
-
-		return $weeks;
-	}
-
-	protected function getMimosemestralWeeks($fromWeek) {
-		$weeks = array();
-		for ($i=self::WEEKS_SEMESTRAL_COUNT+1,$weekNumber = $fromWeek;$i <= self::WEEKS_SEMESTRAL_COUNT+10;$i++,$weekNumber++) {
-			$realStart = $weekNumber % self::WEEKS_YEAR_COUNT;
-			$realEnd = ($weekNumber+1) % self::WEEKS_YEAR_COUNT;
-			$yearStart = $realStart != $weekNumber ?$this->getDate('Y')+1 :$this->getDate('Y');
-			$yearEnd = $realEnd != ($weekNumber+1) ?$this->getDate('Y')+1 :$this->getDate('Y');
-			$start = $this->createDateTimeFromWeek($realStart, $yearStart);
-			$end = $this->createDateTimeFromWeek($realEnd, $yearEnd);
-			if (!$start || !$end) continue;
-			$week = array(
-				'id' => $i,
-				'title' => $start->format('j.n.').' - '.$end->format('j.n.'),
-				'week_number' => $weekNumber,
-				'start' => $start,
-				'end' => $end->sub(\DateInterval::createFromDateString('1 days')),
-			);
 			$weeks[$weekNumber] = $week;
 		}
 
@@ -234,18 +182,20 @@ _dBar($events);
 	 */
 	protected function createDateTimeFromWeek($week, $year = false) {
 		if ($year === false) $year = $this->getDate('Y');
-		$date = \DateTime::createFromFormat('Y-m-D', $year.'-01-Mon')->sub(\DateInterval::createFromDateString('4 weeks'));
+
 		$limit = 100;
-		while($limit > 0) {
-			if ($date->format('W') >= $week) {
-				return $date;
-			}
+
+		$date = \DateTime::createFromFormat('Y-m-d', $year.'-01-01');
+		while ($date->format('W') != $week) {
 			$date = $date->add(\DateInterval::createFromDateString('1 weeks'));
-			$limit--;
+			if (!$limit) break;$limit--;
+		}
+		while ($date->format('w') != 1) {
+			$date = $date->sub(\DateInterval::createFromDateString('1 days'));
+			if (!$limit) break;$limit--;
 		}
 
-		// Když se něco posere tak aby se nezacyklilo
-		return false;
+		return $date;
 	}
 
 
@@ -255,7 +205,7 @@ _dBar($events);
 	 */
 	protected function getDate($params = false) {
 		$date = new DateTime();
-		//$date = \DateTime::createFromFormat('Y-m-d', '2011-03-12');
+		//$date = \DateTime::createFromFormat('Y-m-d', '2012-12-01');
 		if ($params === false) return $date;
 
 		return $date->format($params);
