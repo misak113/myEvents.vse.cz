3c843f8fc25f6fd4e67c8772c60e0eba168c2075
xmacj64 <xmacj64@ea3fb1bf-ebfe-4ede-80e0-45d1aed49344>
2013-01-26 22:40:09 +0000

    - úprava registrací
    git-svn-id: https://4it445.vse.cz/svn/zs1213_c@380 ea3fb1bf-ebfe-4ede-80e0-45d1aed49344

--- trunk/application/modules/default/controllers/UserController.php
+++ trunk/application/modules/default/controllers/UserController.php
@@ -68,47 +68,53 @@ class UserController extends BaseController {
         if ($this->_request->isPost()) {
             if ($form->isValid($this->_request->getPost())) { // Validace prošla, jde se na registraci
                 $formValues = $form->getValues();
-                $password = new My_Password($formValues["password1"]);
-
-                // Uložit záznam do tabulky user
-                $user = $this->userTable->createRow();
-                $user->email = $formValues["email"];
-                $user->first_name = $formValues["name"];
-                $user->last_name = $formValues["surname"];
-				$user->last_login_date = new Zend_Db_Expr("NOW()");
-				$user->last_login_ip = $this->getRequest()->getServer('REMOTE_ADDR');
-                $user->save();
-
-                // Uložit záznam do tabulky authenticate
-                $auth = $this->authenticateTable->createRow();
-                $auth->created = new Zend_Db_Expr("NOW()");
-                $auth->identity = $user->email;
-                $auth->verification = $password->getDHash();
-                $auth->user_id = $user->user_id;
-                $auth->authenticate_provides_id = Application_Plugin_DbAuth::AUTHENTICATE_PROVIDE_EMAIL;
-				$auth->active = 0;
-                $auth->save();
-
-                // Aktivační e-mail
-                $text = "Dobrý den,\n";
-                $text .= "zasíláme Vám registrační údaje na portálu MyEvents.vse.cz.\n";
-                $text .= "Váš účet je ještě třeba aktivovat. Aktivaci provedete kliknutím na odkaz, který je přiložený níže. Pokud Vám na odkaz nejde kliknout, překopírujte ho do adresního řádku svého webového prohlížeče.\n\n";
-                $text .= "Přihlašovací e-mail: " . $auth->identity . "\n";
-                $text .= "Heslo: " . $formValues["password1"] . "\n";
-                $text .= "Aktivační odkaz: http://" . $_SERVER['SERVER_NAME'] . "/aktivace/" . $auth->authenticate_id . "/" . substr($auth->verification, 0, 10) . "\n\n";
-                $text .= "Doufáme, že se Vám bude na portálu MyEvents líbit a že pro vás bude užitečným :).";
-
-                $mail = new Zend_Mail("utf-8");
-                $mail->addTo($user->email, $user->first_name . " " . $user->last_name);
-                $mail->setSubject("Registrace na MyEvents");
-                $mail->setFrom("no-reply@vse.cz", "MyEvents");
-                $mail->setBodyText($text);
-                $mail->send();
+                $this->doRegistration($formValues["email"], $formValues["password1"], $formValues["name"], $formValues["surname"], true);
 
                 // Přesměrování a zpráva
                 $this->flashMessage("Vaše registrace byla úspěšná. Posledním krokem k dokončení registrace je ale ještě aktivace Vašeho účtu. Instrukce naleznete v e-mailu, který jsme Vám zaslali.", self::FLASH_INFO);
                 $this->_helper->redirector->gotoRouteAndExit(array(), "eventList");
             }
+        }
+    }
+    
+    private function doRegistration($email, $password, $name, $surname, $activationRequired) {
+        $password = new My_Password($password);
+
+        // Uložit záznam do tabulky user
+        $user = $this->userTable->createRow();
+        $user->email = $email;
+        $user->first_name =$name ;
+        $user->last_name = $surname;
+        $user->last_login_date = new Zend_Db_Expr("NOW()");
+        $user->last_login_ip = $this->getRequest()->getServer('REMOTE_ADDR');
+        $user->save();
+
+        // Uložit záznam do tabulky authenticate
+        $auth = $this->authenticateTable->createRow();
+        $auth->created = new Zend_Db_Expr("NOW()");
+        $auth->identity = $user->email;
+        $auth->verification = $password->getDHash();
+        $auth->user_id = $user->user_id;
+        $auth->authenticate_provides_id = Application_Plugin_DbAuth::AUTHENTICATE_PROVIDE_EMAIL;
+        $auth->active = $activationRequired ? 0 : 1;
+        $auth->save();
+
+        // Aktivační e-mail
+        if ($activationRequired) {
+            $text = "Dobrý den,\n";
+            $text .= "zasíláme Vám registrační údaje na portálu myEvents.vse.cz.\n";
+            $text .= "Váš účet je ještě třeba aktivovat. Aktivaci provedete kliknutím na odkaz, který je přiložený níže. Pokud Vám na odkaz nejde kliknout, překopírujte ho do adresního řádku svého webového prohlížeče.\n\n";
+            $text .= "Přihlašovací e-mail: " . $auth->identity . "\n";
+            $text .= "Heslo: " . $formValues["password1"] . "\n";
+            $text .= "Aktivační odkaz: http://" . $_SERVER['SERVER_NAME'] . "/aktivace/" . $auth->authenticate_id . "/" . substr($auth->verification, 0, 10) . "\n\n";
+            $text .= "Doufáme, že se Vám bude na portálu MyEvents líbit a že pro vás bude užitečným :).";
+
+            $mail = new Zend_Mail("utf-8");
+            $mail->addTo($user->email, $user->first_name . " " . $user->last_name);
+            $mail->setSubject("Registrace na MyEvents");
+            $mail->setFrom("no-reply@vse.cz", "MyEvents");
+            $mail->setBodyText($text);
+            $mail->send();
         }
     }
 
