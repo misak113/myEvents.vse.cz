2d77fdb627e85837275194f3fbbb5e90b5f6cbeb
xmacj64 <xmacj64@ea3fb1bf-ebfe-4ede-80e0-45d1aed49344>
2013-02-14 21:20:49 +0000

    git-svn-id: https://4it445.vse.cz/svn/zs1213_c@467 ea3fb1bf-ebfe-4ede-80e0-45d1aed49344

--- trunk/library/My/GcmMessanger.php
+++ trunk/library/My/GcmMessanger.php
@@ -94,7 +94,7 @@ class My_GcmMessanger {
             
             $i++;
         }
-
+print_r($results);
         // Work out results
         foreach ($results as $res) {
             if ($res["type"] == "error" && ($res["content"] == "NotRegistered" || $res["content"] == "InvalidRegistration")) {
