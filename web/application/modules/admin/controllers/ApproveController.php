df9c5bd31484348be2699bdf5d6c7db29bc2b5ed
fandič <toman.frantisek@gmail.com>
2013-04-20 18:54:11 +0200

    schvalovani a kontrola vice akci najednou
    Signed-off-by: fandič <toman.frantisek@gmail.com>

--- web/application/modules/admin/controllers/ApproveController.php
+++ web/application/modules/admin/controllers/ApproveController.php
@@ -48,9 +48,6 @@ class Admin_ApproveController extends BaseController {
     }
     
     public function listAction() {
-        //TODO overit prava
-        barDump($this->user->getRoles());
-        barDump($this->user);
         $this->template->title = $this->titleLoader->getTitle('Admin:Approve:list');
 
         $where = $this->eventTable->select()
@@ -66,9 +63,8 @@ class Admin_ApproveController extends BaseController {
         if($this->user->isAllowed('admin.approve', 'control')){
             $where->orWhere('controlled is null');
         }
-		$where->orWhere('0)');
+        $where->orWhere('0)');
 
-		barDump($where);
         $events = $this->eventTable->fetchAll($where);
         
         $this->template->events = $events;
@@ -116,6 +112,67 @@ class Admin_ApproveController extends BaseController {
 			'default',
 			true);
 	}
+        
+        public function approveallAction(){
+            if($this->_request->isPost()){
+                $date = date('Y-m-d H:i:s', time());
+                $disApproved = $this->getDisApproved();
+                
+                foreach($disApproved as $record){
+                    $record->approved = $date;
+                    $record->save();
+                }
+            }
+            
+            $this->flashMessage("Všechny události jsou schváleny.", self::FLASH_INFO);
+            
+            
+            $this->_helper->redirector->gotoRoute(array('module' => 'admin',
+                'controller' => 'approve',
+                'action' => 'list'),
+            'default',
+            true);
+        }
+        
+        protected function getDisApproved(){
+            $where = $this->eventTable->select()
+                ->where('active = 1')
+                ->where('ready_to_approve = 1')
+                ->where('(timeend > NOW() OR timeend IS NULL)')
+                ->where('approved is null');
+
+            return $this->eventTable->fetchAll($where);
+        }
+        
+        public function controlallAction() {
+            if($this->_request->isPost()){
+                $date = date('Y-m-d H:i:s', time());
+                $notControlled = $this->getNotControlled();
+                
+                foreach($notControlled as $record){
+                    $record->controlled = $date;
+                    $record->save();
+                }
+            }
+            
+            $this->flashMessage("Všechny události jsou zkontrolovány.", self::FLASH_INFO);
+            
+            $this->_helper->redirector->gotoRoute(array('module' => 'admin',
+                'controller' => 'approve',
+                'action' => 'list'),
+            'default',
+            true);
+        }
+        
+        protected function getNotControlled(){
+            $where = $this->eventTable->select()
+                ->where('active = 1')
+                ->where('ready_to_approve = 1')
+                ->where('(timeend > NOW() OR timeend IS NULL)')
+                ->where('controlled is null');
+
+            return $this->eventTable->fetchAll($where);
+        }
     
 }
 
